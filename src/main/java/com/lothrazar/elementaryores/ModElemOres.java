f8584400ed539bdc58e9a524f7e8c75abd6310e0
lothrazar <samson.bassett@gmail.com>
2022-06-22 16:48:06 -0700

    1.19 port game runs but worldgen fails since forge removed biome event

--- src/main/java/com/lothrazar/elementaryores/ModElemOres.java
+++ src/main/java/com/lothrazar/elementaryores/ModElemOres.java
@@ -1,20 +1,36 @@
 package com.lothrazar.elementaryores;
 
+import com.lothrazar.elementaryores.world.BiomeLoader;
 import com.lothrazar.elementaryores.world.ClientOreRegistry;
 import com.lothrazar.elementaryores.world.ConfigHandler;
 import com.lothrazar.elementaryores.world.WorldGenRegistry;
+import com.mojang.serialization.Codec;
+import com.mojang.serialization.codecs.RecordCodecBuilder;
+import net.minecraft.core.Holder;
+import net.minecraft.core.HolderSet;
+import net.minecraft.data.worldgen.features.OreFeatures;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.tags.BiomeTags;
+import net.minecraft.world.level.biome.Biome;
 import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.common.world.BiomeModifier;
+import net.minecraftforge.common.world.ModifiableBiomeInfo.BiomeInfo;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.DistExecutor;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import net.minecraftforge.fml.loading.FMLPaths;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
 
 @Mod(ModElemOres.MODID)
 public class ModElemOres {
 
   public static final String MODID = "elementaryores";
+  private static final String MODIFY_BIOMES = "modify_biomes";
+  private static final ResourceLocation MODIFY_BIOMES_RL = new ResourceLocation(MODID, MODIFY_BIOMES);
 
   public ModElemOres() {
     IEventBus eventBus = FMLJavaModLoadingContext.get().getModEventBus();
@@ -25,11 +41,51 @@ public class ModElemOres {
     DistExecutor.unsafeRunWhenOn(Dist.CLIENT, () -> () -> {
       eventBus.addListener(ClientOreRegistry::setup);
     });
+    final DeferredRegister<Codec<? extends BiomeModifier>> serializers = DeferredRegister.create(ForgeRegistries.Keys.BIOME_MODIFIER_SERIALIZERS, MODID);
+    IEventBus modBus = FMLJavaModLoadingContext.get().getModEventBus();
+    serializers.register(modBus);
+    serializers.register(MODIFY_BIOMES, TestModifier::makeCodec);
   }
 
   private void setup(final FMLCommonSetupEvent event) {
     event.enqueueWork(() -> {
       WorldGenRegistry.init();
     });
+  }
+
+  public record TestModifier(HolderSet<Biome> biomes) implements BiomeModifier {
+
+    private static final RegistryObject<Codec<? extends BiomeModifier>> SERIALIZER = RegistryObject.create(MODIFY_BIOMES_RL, ForgeRegistries.Keys.BIOME_MODIFIER_SERIALIZERS, MODID);
+
+    @Override
+    public void modify(Holder<Biome> biome, Phase phase, BiomeInfo.Builder builder) {
+      //      NetherFeatures.BASALT_BLOBS
+      OreFeatures fuck;
+      //intln("basdfasdf BREAKPOINT NEVER GETS HIT");
+      if (phase == Phase.MODIFY && biome.is(BiomeTags.IS_NETHER)) {
+        //no more BiomeCategory. only nether. 
+        BiomeLoader.modifyNether(builder.getGenerationSettings());
+      }
+      if (phase == Phase.MODIFY && biome.is(BiomeTags.IS_END)) {
+        //no more BiomeCategory. only nether. 
+        BiomeLoader.modifyNether(builder.getGenerationSettings());
+      }
+    }
+
+    @Override
+    public Codec<? extends BiomeModifier> codec() {
+      return SERIALIZER.get();
+    }
+
+    private static Codec<TestModifier> makeCodec() {
+      //ok a breakpoint gets hit in this one
+      return RecordCodecBuilder.create(builder -> builder
+          .group(
+              Biome.LIST_CODEC.fieldOf("biomes").forGetter(TestModifier::biomes)
+          //              Precipitation.CODEC.fieldOf("precipitation").forGetter(TestModifier::precipitation),
+          //              Codec.INT.fieldOf("water_color").forGetter(TestModifier::waterColor)
+          )
+          .apply(builder, TestModifier::new));
+    }
   }
 }
--- src/main/java/com/lothrazar/elementaryores/world/BiomeLoader.java
+++ src/main/java/com/lothrazar/elementaryores/world/BiomeLoader.java
@@ -1,52 +1,44 @@
 package com.lothrazar.elementaryores.world;
 
-import com.lothrazar.elementaryores.ModElemOres;
-import net.minecraft.world.level.biome.Biome;
 import net.minecraft.world.level.levelgen.GenerationStep.Decoration;
 import net.minecraftforge.common.world.BiomeGenerationSettingsBuilder;
-import net.minecraftforge.event.world.BiomeLoadingEvent;
-import net.minecraftforge.eventbus.api.EventPriority;
-import net.minecraftforge.eventbus.api.SubscribeEvent;
-import net.minecraftforge.fml.common.Mod.EventBusSubscriber;
 
-@EventBusSubscriber(modid = ModElemOres.MODID, bus = EventBusSubscriber.Bus.FORGE)
+//@EventBusSubscriber(modid = ModElemOres.MODID, bus = EventBusSubscriber.Bus.FORGE)
 public class BiomeLoader {
 
-  @SubscribeEvent(priority = EventPriority.HIGH)
-  public static void onBiomeLoading(BiomeLoadingEvent event) {
+  public static void modifyNether(BiomeGenerationSettingsBuilder r) {
     Decoration d = Decoration.UNDERGROUND_ORES;
-    BiomeGenerationSettingsBuilder r = event.getGeneration();
-    if (event.getCategory() == Biome.BiomeCategory.NETHER) {
-      if (ConfigHandler.Nether.EMERALD_VEINSIZE.get() > 0)
-        r.addFeature(d, WorldGenRegistry.EMERALD_NETHER_PLACED);
-      if (ConfigHandler.Nether.DIAMOND_VEINSIZE.get() > 0)
-        r.addFeature(d, WorldGenRegistry.DIAMOND_NETHER_PLACED);
-      if (ConfigHandler.Nether.LAPIS_VEINSIZE.get() > 0)
-        r.addFeature(d, WorldGenRegistry.LAPIS_NETHER_PLACED);
-      if (ConfigHandler.Nether.IRON_VEINSIZE.get() > 0)
-        r.addFeature(d, WorldGenRegistry.IRON_NETHER_PLACED);
-      if (ConfigHandler.Nether.COAL_VEINSIZE.get() > 0)
-        r.addFeature(d, WorldGenRegistry.COAL_NETHER_PLACED);
-      if (ConfigHandler.Nether.COPPER_VEINSIZE.get() > 0)
-        r.addFeature(d, WorldGenRegistry.COPPER_NETHER_PLACED);
-      if (ConfigHandler.Nether.GHAST_VEINSIZE.get() > 0)
-        r.addFeature(d, WorldGenRegistry.GHAST_NETHER_PLACED);
-    }
-    else if (event.getCategory() == Biome.BiomeCategory.THEEND) {
-      if (ConfigHandler.End.REDSTONEVEINSIZE.get() > 0)
-        r.addFeature(d, WorldGenRegistry.REDSTONE_END_PLACED);
-      if (ConfigHandler.End.EMERALDVEINSIZE.get() > 0)
-        r.addFeature(d, WorldGenRegistry.EMERALD_END_PLACED);
-      if (ConfigHandler.End.DIAMONDVEINSIZE.get() > 0)
-        r.addFeature(d, WorldGenRegistry.DIAMOND_END_PLACED);
-      if (ConfigHandler.End.ENDERVEINSIZE.get() > 0)
-        r.addFeature(d, WorldGenRegistry.ENDER_END_PLACED);
-      if (ConfigHandler.End.LAPISVEINSIZE.get() > 0)
-        r.addFeature(d, WorldGenRegistry.LAPIS_END_PLACED);
-      if (ConfigHandler.End.COALVEINSIZE.get() > 0)
-        r.addFeature(d, WorldGenRegistry.COAL_END_PLACED);
-      if (ConfigHandler.End.COPPERVEINSIZE.get() > 0)
-        r.addFeature(d, WorldGenRegistry.COPPER_END_PLACED);
-    }
+    if (ConfigHandler.Nether.EMERALD_VEINSIZE.get() > 0)
+      r.addFeature(d, WorldGenRegistry.EMERALD_NETHER_PLACED);
+    if (ConfigHandler.Nether.DIAMOND_VEINSIZE.get() > 0)
+      r.addFeature(d, WorldGenRegistry.DIAMOND_NETHER_PLACED);
+    if (ConfigHandler.Nether.LAPIS_VEINSIZE.get() > 0)
+      r.addFeature(d, WorldGenRegistry.LAPIS_NETHER_PLACED);
+    if (ConfigHandler.Nether.IRON_VEINSIZE.get() > 0)
+      r.addFeature(d, WorldGenRegistry.IRON_NETHER_PLACED);
+    if (ConfigHandler.Nether.COAL_VEINSIZE.get() > 0)
+      r.addFeature(d, WorldGenRegistry.COAL_NETHER_PLACED);
+    if (ConfigHandler.Nether.COPPER_VEINSIZE.get() > 0)
+      r.addFeature(d, WorldGenRegistry.COPPER_NETHER_PLACED);
+    if (ConfigHandler.Nether.GHAST_VEINSIZE.get() > 0)
+      r.addFeature(d, WorldGenRegistry.GHAST_NETHER_PLACED);
+  }
+
+  public static void modifyEnd(BiomeGenerationSettingsBuilder r) {
+    Decoration d = Decoration.UNDERGROUND_ORES;
+    if (ConfigHandler.End.REDSTONEVEINSIZE.get() > 0)
+      r.addFeature(d, WorldGenRegistry.REDSTONE_END_PLACED);
+    if (ConfigHandler.End.EMERALDVEINSIZE.get() > 0)
+      r.addFeature(d, WorldGenRegistry.EMERALD_END_PLACED);
+    if (ConfigHandler.End.DIAMONDVEINSIZE.get() > 0)
+      r.addFeature(d, WorldGenRegistry.DIAMOND_END_PLACED);
+    if (ConfigHandler.End.ENDERVEINSIZE.get() > 0)
+      r.addFeature(d, WorldGenRegistry.ENDER_END_PLACED);
+    if (ConfigHandler.End.LAPISVEINSIZE.get() > 0)
+      r.addFeature(d, WorldGenRegistry.LAPIS_END_PLACED);
+    if (ConfigHandler.End.COALVEINSIZE.get() > 0)
+      r.addFeature(d, WorldGenRegistry.COAL_END_PLACED);
+    if (ConfigHandler.End.COPPERVEINSIZE.get() > 0)
+      r.addFeature(d, WorldGenRegistry.COPPER_END_PLACED);
   }
 }
