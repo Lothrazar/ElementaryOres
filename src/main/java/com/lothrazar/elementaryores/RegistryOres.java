d5a9a4b9fe6b5f651dcbfd438ff4c2838f4bea0b
Sam <lothrazar@hotmail.com>
2026-08-12 10:21:02 -0700

    porting 26.1

--- src/main/java/com/lothrazar/elementaryores/RegistryOres.java
+++ src/main/java/com/lothrazar/elementaryores/RegistryOres.java
@@ -18,45 +18,45 @@ public class RegistryOres {
 
 //  private static final ResourceKey<CreativeModeTab> TAB_KEY = ResourceKey.create(Registries.CREATIVE_MODE_TAB, Identifier.fromNamespaceAndPath(ModOres.MODID, "tab"));
   public static final DeferredRegister<CreativeModeTab> TABS = DeferredRegister.create(Registries.CREATIVE_MODE_TAB, ModOres.MODID);
-  public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(Registries.ITEM, ModOres.MODID);
-  public static final DeferredRegister<Block> BLOCKS = DeferredRegister.create(Registries.BLOCK, ModOres.MODID);
+  public static final DeferredRegister.Items ITEMS = DeferredRegister.createItems(ModOres.MODID);
+  public static final DeferredRegister.Blocks BLOCKS = DeferredRegister.createBlocks(ModOres.MODID);
 
   //a randomizer?
   // GUNPOWDER  // GLOWSTONE DUST // GHAST TEAR // BLAZE POWDER
-  public static final DeferredHolder<Block, Block> COAL_NETHER = BLOCKS.register("ore_coal_nether", () -> new BlockFlib(Block.Properties.of().strength(Blocks.COAL_ORE.defaultBlockState().destroySpeed)));
-  public static final DeferredHolder<Block, Block> COPPER_NETHER = BLOCKS.register("ore_copper_nether", () -> new BlockFlib(Block.Properties.of().strength(Blocks.COPPER_ORE.defaultBlockState().destroySpeed)));
-  public static final DeferredHolder<Block, Block> IRON_NETHER = BLOCKS.register("ore_iron_nether", () -> new Block(BlockFlib.Properties.of().strength(Blocks.IRON_ORE.defaultBlockState().destroySpeed)));
-  public static final DeferredHolder<Block, Block> EMERALD_NETHER = BLOCKS.register("ore_emerald_nether", () -> new BlockFlib(Block.Properties.of().strength(Blocks.EMERALD_ORE.defaultBlockState().destroySpeed)));
-  public static final DeferredHolder<Block, Block> LAPIS_NETHER = BLOCKS.register("ore_lapis_nether", () -> new BlockFlib(Block.Properties.of().strength(Blocks.LAPIS_ORE.defaultBlockState().destroySpeed)));
-  public static final DeferredHolder<Block, Block> DIAMOND_NETHER = BLOCKS.register("ore_diamond_nether", () -> new BlockFlib(Block.Properties.of().strength(Blocks.DIAMOND_ORE.defaultBlockState().destroySpeed)));
+  public static final DeferredHolder<Block, Block> COAL_NETHER = BLOCKS.registerBlock("ore_coal_nether", props -> new BlockFlib(props.strength(Blocks.COAL_ORE.defaultBlockState().destroySpeed)));
+  public static final DeferredHolder<Block, Block> COPPER_NETHER = BLOCKS.registerBlock("ore_copper_nether", props -> new BlockFlib(props.strength(Blocks.COPPER_ORE.defaultBlockState().destroySpeed)));
+  public static final DeferredHolder<Block, Block> IRON_NETHER = BLOCKS.registerBlock("ore_iron_nether", props -> new Block(props.strength(Blocks.IRON_ORE.defaultBlockState().destroySpeed)));
+  public static final DeferredHolder<Block, Block> EMERALD_NETHER = BLOCKS.registerBlock("ore_emerald_nether", props -> new BlockFlib(props.strength(Blocks.EMERALD_ORE.defaultBlockState().destroySpeed)));
+  public static final DeferredHolder<Block, Block> LAPIS_NETHER = BLOCKS.registerBlock("ore_lapis_nether", props -> new BlockFlib(props.strength(Blocks.LAPIS_ORE.defaultBlockState().destroySpeed)));
+  public static final DeferredHolder<Block, Block> DIAMOND_NETHER = BLOCKS.registerBlock("ore_diamond_nether", props -> new BlockFlib(props.strength(Blocks.DIAMOND_ORE.defaultBlockState().destroySpeed)));
   //
-  public static final DeferredHolder<Block, Block> COAL_END = BLOCKS.register("ore_coal_end", () -> new BlockFlib(Block.Properties.of().strength(Blocks.COAL_ORE.defaultBlockState().destroySpeed)));
-  public static final DeferredHolder<Block, Block> COPPER_END = BLOCKS.register("ore_copper_end", () -> new BlockFlib(Block.Properties.of().strength(Blocks.COPPER_ORE.defaultBlockState().destroySpeed)));
-  public static final DeferredHolder<Block, Block> REDSTONE_END = BLOCKS.register("ore_redstone_end", () -> new BlockFlib(Block.Properties.of().strength(Blocks.REDSTONE_ORE.defaultBlockState().destroySpeed)));
-  public static final DeferredHolder<Block, Block> EMERALD_END = BLOCKS.register("ore_emerald_end", () -> new BlockFlib(Block.Properties.of().strength(Blocks.EMERALD_ORE.defaultBlockState().destroySpeed)));
-  public static final DeferredHolder<Block, Block> LAPIS_END = BLOCKS.register("ore_lapis_end", () -> new BlockFlib(Block.Properties.of().strength(Blocks.LAPIS_ORE.defaultBlockState().destroySpeed)));
-  public static final DeferredHolder<Block, Block> DIAMOND_END = BLOCKS.register("ore_diamond_end", () -> new BlockFlib(Block.Properties.of().strength(Blocks.DIAMOND_ORE.defaultBlockState().destroySpeed)));
+  public static final DeferredHolder<Block, Block> COAL_END = BLOCKS.registerBlock("ore_coal_end", props -> new BlockFlib(props.strength(Blocks.COAL_ORE.defaultBlockState().destroySpeed)));
+  public static final DeferredHolder<Block, Block> COPPER_END = BLOCKS.registerBlock("ore_copper_end", props -> new BlockFlib(props.strength(Blocks.COPPER_ORE.defaultBlockState().destroySpeed)));
+  public static final DeferredHolder<Block, Block> REDSTONE_END = BLOCKS.registerBlock("ore_redstone_end", props -> new BlockFlib(props.strength(Blocks.REDSTONE_ORE.defaultBlockState().destroySpeed)));
+  public static final DeferredHolder<Block, Block> EMERALD_END = BLOCKS.registerBlock("ore_emerald_end", props -> new BlockFlib(props.strength(Blocks.EMERALD_ORE.defaultBlockState().destroySpeed)));
+  public static final DeferredHolder<Block, Block> LAPIS_END = BLOCKS.registerBlock("ore_lapis_end", props -> new BlockFlib(props.strength(Blocks.LAPIS_ORE.defaultBlockState().destroySpeed)));
+  public static final DeferredHolder<Block, Block> DIAMOND_END = BLOCKS.registerBlock("ore_diamond_end", props -> new BlockFlib(props.strength(Blocks.DIAMOND_ORE.defaultBlockState().destroySpeed)));
   //
-  public static final DeferredHolder<Block, Block> ENDER_END = BLOCKS.register("ore_ender_end", () -> new BlockFlib(Block.Properties.of().strength(Blocks.DIAMOND_ORE.defaultBlockState().destroySpeed)));
-  public static final DeferredHolder<Block, Block> GHAST_NETHER = BLOCKS.register("ore_ghast_nether", () -> new BlockFlib(Block.Properties.of().strength(Blocks.DIAMOND_ORE.defaultBlockState().destroySpeed)));
+  public static final DeferredHolder<Block, Block> ENDER_END = BLOCKS.registerBlock("ore_ender_end", props -> new BlockFlib(props.strength(Blocks.DIAMOND_ORE.defaultBlockState().destroySpeed)));
+  public static final DeferredHolder<Block, Block> GHAST_NETHER = BLOCKS.registerBlock("ore_ghast_nether", props -> new BlockFlib(props.strength(Blocks.DIAMOND_ORE.defaultBlockState().destroySpeed)));
 
   static {
-    ITEMS.register("ore_coal_nether", () -> new BlockItemFlib(COAL_NETHER.get(), new Item.Properties().useBlockDescriptionPrefix()));
-    ITEMS.register("ore_copper_nether", () -> new BlockItemFlib(COPPER_NETHER.get(), new Item.Properties().useBlockDescriptionPrefix()));
-    ITEMS.register("ore_iron_nether", () -> new BlockItemFlib(IRON_NETHER.get(), new Item.Properties().useBlockDescriptionPrefix()));
-    ITEMS.register("ore_emerald_nether", () -> new BlockItemFlib(EMERALD_NETHER.get(), new Item.Properties().useBlockDescriptionPrefix()));
-    ITEMS.register("ore_lapis_nether", () -> new BlockItemFlib(LAPIS_NETHER.get(), new Item.Properties().useBlockDescriptionPrefix()));
-    ITEMS.register("ore_diamond_nether", () -> new BlockItemFlib(DIAMOND_NETHER.get(), new Item.Properties().useBlockDescriptionPrefix()));
+    ITEMS.registerItem("ore_coal_nether", props -> new BlockItemFlib(COAL_NETHER.get(), props.useBlockDescriptionPrefix()));
+    ITEMS.registerItem("ore_copper_nether", props -> new BlockItemFlib(COPPER_NETHER.get(), props.useBlockDescriptionPrefix()));
+    ITEMS.registerItem("ore_iron_nether", props -> new BlockItemFlib(IRON_NETHER.get(), props.useBlockDescriptionPrefix()));
+    ITEMS.registerItem("ore_emerald_nether", props -> new BlockItemFlib(EMERALD_NETHER.get(), props.useBlockDescriptionPrefix()));
+    ITEMS.registerItem("ore_lapis_nether", props -> new BlockItemFlib(LAPIS_NETHER.get(), props.useBlockDescriptionPrefix()));
+    ITEMS.registerItem("ore_diamond_nether", props -> new BlockItemFlib(DIAMOND_NETHER.get(), props.useBlockDescriptionPrefix()));
     //
-    ITEMS.register("ore_coal_end", () -> new BlockItemFlib(COAL_END.get(), new Item.Properties().useBlockDescriptionPrefix()));
-    ITEMS.register("ore_copper_end", () -> new BlockItemFlib(COPPER_END.get(), new Item.Properties().useBlockDescriptionPrefix()));
-    ITEMS.register("ore_redstone_end", () -> new BlockItemFlib(REDSTONE_END.get(), new Item.Properties().useBlockDescriptionPrefix()));
-    ITEMS.register("ore_emerald_end", () -> new BlockItemFlib(EMERALD_END.get(), new Item.Properties().useBlockDescriptionPrefix()));
-    ITEMS.register("ore_lapis_end", () -> new BlockItemFlib(LAPIS_END.get(), new Item.Properties().useBlockDescriptionPrefix()));
-    ITEMS.register("ore_diamond_end", () -> new BlockItemFlib(DIAMOND_END.get(), new Item.Properties().useBlockDescriptionPrefix()));
+    ITEMS.registerItem("ore_coal_end", props -> new BlockItemFlib(COAL_END.get(), props.useBlockDescriptionPrefix()));
+    ITEMS.registerItem("ore_copper_end", props -> new BlockItemFlib(COPPER_END.get(), props.useBlockDescriptionPrefix()));
+    ITEMS.registerItem("ore_redstone_end", props -> new BlockItemFlib(REDSTONE_END.get(), props.useBlockDescriptionPrefix()));
+    ITEMS.registerItem("ore_emerald_end", props -> new BlockItemFlib(EMERALD_END.get(), props.useBlockDescriptionPrefix()));
+    ITEMS.registerItem("ore_lapis_end", props -> new BlockItemFlib(LAPIS_END.get(), props.useBlockDescriptionPrefix()));
+    ITEMS.registerItem("ore_diamond_end", props -> new BlockItemFlib(DIAMOND_END.get(), props.useBlockDescriptionPrefix()));
     //
-    ITEMS.register("ore_ender_end", () -> new BlockItemFlib(ENDER_END.get(), new Item.Properties().useBlockDescriptionPrefix()));
-    ITEMS.register("ore_ghast_nether", () -> new BlockItemFlib(GHAST_NETHER.get(), new Item.Properties().useBlockDescriptionPrefix()));
+    ITEMS.registerItem("ore_ender_end", props -> new BlockItemFlib(ENDER_END.get(), props.useBlockDescriptionPrefix()));
+    ITEMS.registerItem("ore_ghast_nether", props -> new BlockItemFlib(GHAST_NETHER.get(), props.useBlockDescriptionPrefix()));
   }
 
   static {
